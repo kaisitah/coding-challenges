58a9b8562664a83731472c24e6698526ab9174b3
Kaisa Haverinen <kaisa.haverinen@iki.fi>
2022-09-20 00:39:24 +0300

    Add js files

---
+++ capitalizeName.js
@@ -0,0 +1,21 @@
+const capitalizeName = (name) => {
+    console.log(name);
+    
+    /*const firstLetter = name.slice(0, 1);
+    console.log(firstLetter);
+    
+    const restOfName = name.slice(1);
+    console.log(restOfName);
+    
+    const capitalizedName = firstLetter.toUpperCase() + restOfName.toLowerCase(); */
+    
+    const capitalizedName = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+    console.log(`Capitalized: ${capitalizedName}`);
+    return(capitalizedName);
+}
+
+capitalizeName("KAISA"); // test case 1
+capitalizeName("kaisa"); // test case 2
+capitalizeName("kAISA"); // test case 3
+capitalizeName("KaIsa"); // test case 4
+capitalizeName("kAiSa"); // test case 5
